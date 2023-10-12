bdfef6f0fc65398fee1e690e65c1d29e0cc2ace6
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-10-12 15:49:38 -0400

    made slight change

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
                 <svg className="w-2.5 h-2.5 mr-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
                     <path d="M11 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm8.585 1.189a.994.994 0 0 0-.9-.138l-2.965.983a1 1 0 0 0-.685.949v8a1 1 0 0 0 .675.946l2.965 1.02a1.013 1.013 0 0 0 1.032-.242A1 1 0 0 0 20 12V2a1 1 0 0 0-.415-.811Z"/>
                 </svg>
-                Tutorial
+                Blog
             </a>
             <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2"> My Universe: Dive into a Showcase of Innovative Projects!</h1>
             <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">Take a journey through my diverse portfolio, each project tells a unique story of problem-solving, creativity, and a commitment to excellence. Let the code speak for itself.</p>
